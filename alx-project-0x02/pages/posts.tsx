567c07606d0ae3f0a4739d0636aeb818158cb7d9
Emmanuel Godwin <nicensix@gmail.com>
2025-06-15 07:57:03 +0300

    SSG

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,26 +1,23 @@
-import { useEffect, useState } from "react";
 import Header from "@/components/layout/Header";
 import PostCard from "@/components/common/PostCard";
 import { type PostProps } from "@/interfaces";
 
-export default function Posts() {
-  const [posts, setPosts] = useState<PostProps[]>([]);
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=6");
+  const data = await res.json();
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=6");
-      const data = await res.json();
-      const formatted = data.map((post: any) => ({
-        title: post.title,
-        content: post.body,
-        userId: post.userId,
-      }));
-      setPosts(formatted);
-    };
+  const posts: PostProps[] = data.map((post: any) => ({
+    title: post.title,
+    content: post.body,
+    userId: post.userId,
+  }));
 
-    fetchPosts();
-  }, []);
+  return {
+    props: { posts },
+  };
+};
 
+export default function Posts({ posts }: { posts: PostProps[] }) {
   return (
     <div className="min-h-screen bg-gray-100">
       <Header />
